d0bdba075999fa8a1e9ee3efd5395333e7613e57
DP <alejandrop@tecnologicoismac.edu.ec>
2025-05-16 10:53:17 -0500

    Mi primer versionamiento

---
+++ src/main/java/com/distribuida/principal/ClientePrincipal.java
@@ -0,0 +1,13 @@
+package com.distribuida.principal;
+
+import com.distribuida.entities.Cliente;
+
+public class ClientePrincipal {
+
+    public static void main(String[] args){
+
+        Cliente cliente = new Cliente(1, "17434123312","Juan", "Taipa", "Av. Americas", "077899955", "dasdeasd@gmail.com");
+
+    }
+
+}
---
+++ src/main/java/com/distribuida/principal/FacturaPrincipal.java
@@ -0,0 +1,26 @@
+package com.distribuida.principal;
+
+
+
+import com.distribuida.entities.Cliente;
+import com.distribuida.entities.Factura;
+
+import java.util.Date;
+
+public class FacturaPrincipal {
+
+    public static void main(String[] args) {
+
+        Factura factura = new Factura();
+        Cliente cliente = new Cliente(1, "17434123312","Juan", "Taipa", "Av. Americas", "077899955", "dasdeasd@gmail.com");
+
+        factura.setIdFactura(1);
+        factura.setNumFactura("FAC-0001");
+        factura.setFecha(new Date());
+        factura.setTotalNeto(100.00);
+        factura.setIva(115.00);
+        factura.setCliente(cliente);
+
+    }
+
+}
